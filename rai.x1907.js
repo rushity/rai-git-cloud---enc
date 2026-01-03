911ebf3fad89e209992989d7b5f7d8e71d15d23d
rushity <rushityadavds@gmail.com>
2026-01-03 15:16:15 +0530

    modified:   rai.x1907.js

--- rai.x1907.js
+++ rai.x1907.js
@@ -16,7 +16,7 @@ document.body.insertAdjacentHTML("beforeend", `
   <!-- FACE (slightly narrower & centered better) -->
   <rect
     x="9"
-    y="17"
+    y="16"
     width="41"
     height="36"
     rx="9"
